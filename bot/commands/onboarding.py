eaec8ad39f5ecc739cbcbada92d41d21530e39dd
Pokken <135404378+pokkeen@users.noreply.github.com>
2026-07-27 14:34:03 -0500

    Whoopsy Part 2

--- bot/commands/onboarding.py
+++ bot/commands/onboarding.py
@@ -16,7 +16,7 @@ class test_modal(ui.Modal, title='test input 2'):
     async def on_submit(self, interaction: discord.Interaction):
 
         await interaction.response.send_message(f'Thanks for your response!', ephemeral=True)
-        await interaction.followup.send_message(f'Here is your data {self.firstName}, {self.lastName}, {self.emailAddress}, {self.classStanding}')
+        await interaction.followup.send(f'Here is your data {self.firstName}, {self.lastName}, {self.emailAddress}, {self.classStanding}')
 
 @onboarding_group.command(name='input',description='using the first inputmethod')
 async def onboarding_input(interaction:discord.Interaction):
