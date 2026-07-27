38336851bad0b4885ed0d0878aa479f4c22dd0e7
Pokken <135404378+pokkeen@users.noreply.github.com>
2026-07-27 14:05:47 -0500

    i wanna die

--- bot/commands/misc.py
+++ bot/commands/misc.py
@@ -1,7 +1,7 @@
 import discord
 from discord import app_commands
 
-@app_commands.command(name='goodbye', description='Say hello to the bot.')
+@app_commands.command(name='hello', description='yuh')
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message('I cant do anything!!', ephemeral=True)
+    await interaction.response.send_message('I can do anything!!', ephemeral=True)
 
