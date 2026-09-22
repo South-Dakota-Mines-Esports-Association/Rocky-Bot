2a25a671a29cf87b043f65e1e7f1da7bfb27879b
Pokken <135404378+pokkeen@users.noreply.github.com>
2026-09-21 20:59:29 -0600

    Update misc.py
    Changed something slight to test it

--- bot/commands/misc.py
+++ bot/commands/misc.py
@@ -3,5 +3,5 @@ from discord import app_commands
 
 @app_commands.command(name='hello', description='yuh')
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message('I can do anything!!', ephemeral=True)
+    await interaction.response.send_message('I can do anything! PART 2', ephemeral=True)
 
