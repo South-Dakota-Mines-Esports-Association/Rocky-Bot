c926b9d9ef724fef33b0f577dd6667b35e1ff5cd
Pokken <135404378+pokkeen@users.noreply.github.com>
2026-09-21 21:29:51 -0600

    Update onboarding.py

--- bot/commands/onboarding.py
+++ bot/commands/onboarding.py
@@ -44,6 +44,7 @@ class test_modal(ui.Modal, title='test input 2'):
             '/member',
             params={},
             request_body = {
+                'discord_username': interaction.user.name,
                 'first_name' : self.firstName.value, 
                 'last_name' : self.lastName.value, 
                 'email_address' : self.emailAddress.value
